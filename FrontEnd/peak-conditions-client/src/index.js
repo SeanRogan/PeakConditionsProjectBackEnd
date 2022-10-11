19605c5ced55db770a6dcc6ee5af85b08478f5b8
seanrogan <srogan88@gmail.com>
2022-10-11 19:42:02 -0400

    added bootstrap

--- FrontEnd/peak-conditions-client/src/index.js
+++ FrontEnd/peak-conditions-client/src/index.js
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import './index.css';
 import App from './App';
 import {render} from "react-dom";
-
 render(<App /> , document.getElementById('root'));
 
